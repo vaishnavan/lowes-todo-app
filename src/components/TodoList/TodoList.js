2326edd22cfb461dc70eb6ea08eaf0dd9a4df8b8
vaishnavanm5 <vaishnavanm5@gmail.com>
2023-06-03 19:22:53 +0530

    feat: compeleted all functionality and checkbox feature

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import {
   Container,
   Typography,
@@ -14,17 +14,22 @@ import {
 import {
   Add as AddIcon,
   Delete as DeleteIcon,
-  Update as UpdateIcon,
+  Edit as UpdateIcon,
+  CheckBoxOutlineBlankOutlined,
+  CheckBox,
 } from '@mui/icons-material';
 import { todoContext } from '../../context/TodoProvider';
 import {
   createTodoService,
   deleteTodoService,
+  updateTodoService,
 } from '../../services/todo.services';
 
 function TodoApp() {
   const { todoData, setTodoData, itemName, setItemName } =
     useContext(todoContext);
+  const [btnToggle, setBtnToggle] = useState(false);
+  const [editId, setEditId] = useState(null);
 
   const handleChange = (event) => {
     setItemName(event.target.value);
@@ -35,14 +40,32 @@ function TodoApp() {
     const obj = {
       itemName,
     };
-    createTodoService(obj)
-      .then((res) => {
-        setTodoData([...todoData, res.result]);
-        setItemName('');
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    if (editId) {
+      updateTodoService(obj, editId)
+        .then(() => {
+          const updateData = todoData.map((data) => {
+            if (data._id === editId) {
+              return { ...data, itemName };
+            }
+            return data;
+          });
+          setTodoData(updateData);
+          setItemName('');
+          setBtnToggle(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      createTodoService(obj)
+        .then((res) => {
+          setTodoData([...todoData, res.result]);
+          setItemName('');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
 
   const handleRemove = (id) => {
@@ -51,6 +74,34 @@ function TodoApp() {
       .then(() => {
         setTodoData(filterData);
         console.log('deleted successfully');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleEdit = (id) => {
+    const findItem = todoData.find((data) => data._id === id);
+    setItemName(findItem.itemName);
+    setEditId(findItem._id);
+    setBtnToggle(true);
+  };
+
+  const handleCheckBox = (id) => {
+    const findData = todoData.find((findItem) => findItem._id === id);
+    const obj = {
+      itemName: findData.itemName,
+      completed: !findData.completed,
+    };
+    updateTodoService(obj, id)
+      .then(() => {
+        const updateCheck = todoData.map((data) => {
+          if (data._id === id) {
+            return { ...data, completed: !data.completed };
+          }
+          return data;
+        });
+        setTodoData(updateCheck);
       })
       .catch((err) => {
         console.log(err);
@@ -80,7 +131,7 @@ function TodoApp() {
           style={{ width: '10rem' }}
           onClick={handleAdd}
         >
-          Add Item
+          {btnToggle ? 'Update' : 'Add Item'}
         </Button>
       </form>
 
@@ -88,17 +139,40 @@ function TodoApp() {
         {todoData &&
           todoData.map((todo) => (
             <ListItem key={todo._id}>
-              <ListItemText primary={todo.itemName} />
+              <IconButton
+                edge="end"
+                aria-label="check"
+                style={{ margin: '0 2px' }}
+                onClick={() => handleCheckBox(todo._id)}
+              >
+                {todo.completed ? (
+                  <CheckBox />
+                ) : (
+                  <CheckBoxOutlineBlankOutlined />
+                )}
+              </IconButton>
+              <ListItemText
+                primary={todo.itemName}
+                style={
+                  todo.completed
+                    ? { textDecoration: 'line-through' }
+                    : { textDecoration: 'none' }
+                }
+              />
               <ListItemSecondaryAction>
                 <IconButton
                   onClick={() => handleRemove(todo._id)}
                   edge="end"
                   aria-label="delete"
                 >
-                  <DeleteIcon />
+                  <DeleteIcon style={{ color: 'red' }} />
                 </IconButton>
-                <IconButton edge="end" aria-label="update">
-                  <UpdateIcon />
+                <IconButton
+                  onClick={() => handleEdit(todo._id)}
+                  edge="end"
+                  aria-label="update"
+                >
+                  <UpdateIcon style={{ color: 'blue' }} />
                 </IconButton>
               </ListItemSecondaryAction>
             </ListItem>
