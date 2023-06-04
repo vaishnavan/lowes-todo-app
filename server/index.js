a080612f93169fd12f14865db1eee4d6366b69df
vaishnavanm5 <vaishnavanm5@gmail.com>
2023-06-04 17:56:02 +0530

    feat: aPI documentation page creation

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
 
 app.use('/v1/api', require('./routes/todo.routes'));
 
